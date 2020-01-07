bcc7d79a3f3909198442833681731e500a465632
Steve B <sbogucki@mail.usf.edu>
2020-01-07 13:55:57 -0800

    first commit

---
+++ Controllers/CredentialController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using bogoodski2020backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace bogoodski2020backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CredentialController : ControllerBase
+    {        
+        private readonly ILogger<CredentialController> _logger;
+
+        public CredentialController(ILogger<CredentialController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        Credential secret = new Credential
+        {
+            Id = 0,
+            UserName = "steve",
+            PassWord = "1234"
+        };
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Credential credential)
+        {
+
+            if (credential.UserName == secret.UserName && credential.PassWord == secret.PassWord)
+            {
+                string message = "Success!";
+                return Ok(message);
+            }
+
+            if(credential.UserName == secret.UserName && credential.PassWord != secret.PassWord) 
+            {
+                string message = "Wrong password";
+                return Ok(message);
+            }
+
+            if(credential.PassWord == secret.PassWord && credential.UserName != secret.UserName) 
+            {
+                string message = "Wrong username";
+                return Ok(message);
+            }
+
+            else 
+            {
+                return NoContent();
+            } 
+
+              
+                
+        }
+    }
+}
---
+++ Models/Credential.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bogoodski2020backend.Models
+{
+    public class Credential
+    {
+        public Credential() { }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "User Name is Required.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is Required.")]
+        public string PassWord { get; set; }
+    }
+}
